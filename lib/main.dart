01553084a9282d33e08ed5af40db41766c581fd4
sion tchaly <bulalochrist45@gmail.com>
2021-11-17 00:43:24 -0500

    deuxieme version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,50 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(body: Text('Encyclopedie')),
+      home: Scaffold(
+        body: ListView(children: [
+          Image.network(
+            '//cdn.shopify.com/s/files/1/0036/8849/7264/articles/aloe.jpeg?v=1558986665" class="art-Image_Image fde-Fadein-fromTop js-FadeIn fde-Fadein-active',
+            width: 400,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          Text("ALoe Vera",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black)),
+          Text("Species of plant",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black)),
+          Padding(
+              padding: const EdgeInsets.only(right: 0),
+              child: Icon(
+                Icons.center_focus_strong_rounded,
+                color: Colors.black,
+                size: 15,
+              )),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+                "ALoe Vera is a succulent plant of thr genus Aloe. "
+                "An evergreen perennial, it originates from the Arabian Peninsula,"
+                "but grows wild in tropical,semi-tropical, and arid cliamtes around the world"
+                "it is cultivated for agricultural and medicinal uses. "
+                "The species is also used for decorative purposes and grows succesfully"
+                "indoors as a potted plant.",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.black)),
+          ),
+        ]),
+      ),
     );
   }
 }
